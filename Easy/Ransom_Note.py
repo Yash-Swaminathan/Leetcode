0c309953696d34693d1d1116e209ce8eb914568b
Yash-Swaminathan <yashswaminathan1@gmail.com>
2025-09-21 16:09:14 -0400

    Ransom note

---
+++ Easy/Ransom_Note.py
@@ -0,0 +1,50 @@
+# Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+# Each letter in magazine can only be used once in ransomNote.
+
+ 
+
+# Example 1:
+
+# Input: ransomNote = "a", magazine = "b"
+# Output: false
+# Example 2:
+
+# Input: ransomNote = "aa", magazine = "ab"
+# Output: false
+# Example 3:
+
+# Input: ransomNote = "aa", magazine = "aab"
+# Output: true
+ 
+
+# Constraints:
+
+# 1 <= ransomNote.length, magazine.length <= 105
+# ransomNote and magazine consist of lowercase English letters. 
+
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+
+        dictionary = {}
+
+        for char in magazine:
+            if char not in dictionary:
+                dictionary[char] = 1
+            else:
+                dictionary[char] += 1
+
+
+        for char in ransomNote:
+            if char in dictionary and dictionary[char] > 0:
+                dictionary[char] -= 1
+            else:
+                return False
+        return True
+
+        
