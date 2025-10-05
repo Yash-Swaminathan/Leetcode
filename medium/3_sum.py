8761492b12cb1e7c2908365fad64faf054444989
Yash-Swaminathan <yashswaminathan1@gmail.com>
2025-10-04 23:50:38 -0400

    Add implementation of threeSum method to find all unique triplets in an array that sum to zero

--- medium/3_sum.py
+++ medium/3_sum.py
@@ -31,3 +31,32 @@
 # 3 <= nums.length <= 3000
 # -105 <= nums[i] <= 105
 
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        result = []
+
+        for left in range(len(nums) - 2):
+            if left > 0 and nums[left] == nums[left-1]:
+                continue
+            mid = left + 1
+            right = len(nums) - 1
+            while mid < right:
+                curr_sum = nums[left] + nums[right] + nums[mid]
+                if curr_sum < 0:
+                    mid += 1
+                elif curr_sum > 0:
+                    right -= 1
+                else:
+                    result.append([nums[left], nums[mid], nums[right]])
+                    while mid < right and nums[mid] == nums[mid + 1]:
+                        mid += 1
+                    while mid < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    mid += 1
+                    right -= 1
+        return result
