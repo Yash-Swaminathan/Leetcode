55f53a7cc440be7aa71594cf653ee19406ee85d7
Yash-Swaminathan <yashswaminathan1@gmail.com>
2025-10-06 20:39:24 -0400

    Add implementation of invertTree method to invert a binary tree, including handling of edge cases.

--- Easy/invert_binary_tree.py
+++ Easy/invert_binary_tree.py
@@ -22,3 +22,23 @@
 
 # The number of nodes in the tree is in the range [0, 100].
 # -100 <= Node.val <= 100
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: Optional[TreeNode]
+        """
+        if root == None:
+            return root
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+
+        
