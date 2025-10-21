3167a5a2156479894db76b03eb852152a110e141
Yash-Swaminathan <yashswaminathan1@gmail.com>
2025-10-20 22:59:23 -0400

    Add implementation of maxProfit method to calculate maximum profit from stock prices, including logic to sum profits from price increases.

--- medium/buy_sell_stock_pt2.py
+++ medium/buy_sell_stock_pt2.py
@@ -30,3 +30,17 @@
 
 # 1 <= prices.length <= 3 * 104
 # 0 <= prices[i] <= 104
+
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = 0
+        length = len(prices)
+        for i in range(1, length):
+            if prices[i] > prices[i-1]:
+                profit += prices[i] - prices[i-1]
+        return profit
