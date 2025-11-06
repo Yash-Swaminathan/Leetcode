46ada650d882bbe123c633bbc2b59cbc99c82a73
Yash-Swaminathan <yashswaminathan1@gmail.com>
2025-11-05 17:08:17 -0700

    Add implementation of spiral order traversal for a matrix in spiral_matrix.py, including problem statement, examples, and constraints.

--- medium/spiral_matrix.py
+++ medium/spiral_matrix.py
@@ -1,22 +1,58 @@
-Given an m x n matrix, return all elements of the matrix in spiral order.
+# Given an m x n matrix, return all elements of the matrix in spiral order.
 
  
 
-Example 1:
+# Example 1:
 
 
-Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
-Output: [1,2,3,6,9,8,7,4,5]
-Example 2:
+# Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+# Output: [1,2,3,6,9,8,7,4,5]
+# Example 2:
 
 
-Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
-Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+# Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+# Output: [1,2,3,4,8,12,11,10,9,5,6,7]
  
 
-Constraints:
+# Constraints:
 
-m == matrix.length
-n == matrix[i].length
-1 <= m, n <= 10
--100 <= matrix[i][j] <= 100
+# m == matrix.length
+# n == matrix[i].length
+# 1 <= m, n <= 10
+# -100 <= matrix[i][j] <= 100
+
+
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        res = []
+        top, bottom = 0, len(matrix) - 1
+        left, right = 0, len(matrix[0]) - 1
+
+        while top <= bottom and left <= right:
+            # top row 
+            for i in range(left, right + 1):
+                res.append(matrix[top][i])
+            top += 1
+
+            # right column
+            for i in range(top, bottom + 1):
+                res.append(matrix[i][right])
+            right -= 1
+
+            if top <= bottom:
+                #bottom row
+                for i in range(right, left - 1, -1):
+                    res.append(matrix[bottom][i])
+                bottom -= 1
+
+            if left <= right:
+                # Left column
+                for i in range(bottom, top - 1, -1):
+                    res.append(matrix[i][left])
+                left += 1   
+
+        return res
