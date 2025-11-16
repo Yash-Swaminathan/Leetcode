8c75e1bb9797fbc3005980d605f3f41ae312127c
Yash-Swaminathan <yashswaminathan1@gmail.com>
2025-11-15 17:32:14 -0700

    Add implementation of strStr method to find the first occurrence of a substring in a string in Find_First_Ocuurance.py.

--- Easy/Find_First_Ocuurance.py
+++ Easy/Find_First_Ocuurance.py
@@ -20,3 +20,14 @@
 # 1 <= haystack.length, needle.length <= 104
 # haystack and needle consist of only lowercase English characters.
 
+Class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
