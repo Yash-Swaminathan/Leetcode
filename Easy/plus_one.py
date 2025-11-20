7ed84c284e6a1f4f4c380b5241169e482a4cba59
Yash-Swaminathan <yashswaminathan1@gmail.com>
2025-11-19 21:43:12 -0700

    Add implementation of the plusOne method in plus_one.py to increment a large integer represented as an array of digits, including handling of carry-over and edge cases.

--- Easy/plus_one.py
+++ Easy/plus_one.py
@@ -32,3 +32,20 @@
 # 1 <= digits.length <= 100
 # 0 <= digits[i] <= 9
 # digits does not contain any leading 0's.
+
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+
+        i = len(digits) - 1
+        while i >= 0:
+            if digits[i] < 9:  
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+            i -= 1
+        return [1] + digits
+        
