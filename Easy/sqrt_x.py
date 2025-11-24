9e541692ea169ceb92322eda11f2235c48296a97
Yash-Swaminathan <yashswaminathan1@gmail.com>
2025-11-23 19:02:28 -0700

    solution

--- Easy/sqrt_x.py
+++ Easy/sqrt_x.py
@@ -20,3 +20,26 @@
 # Constraints:
 
 # 0 <= x <= 231 - 1
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x == 0:
+            return 0
+        
+        left = 1
+        right = x
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right
+
+
+        
