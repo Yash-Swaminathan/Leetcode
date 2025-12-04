07563980182ce30475464f7d8f5ca5ed503a9938
Yash-Swaminathan <yashswaminathan1@gmail.com>
2025-12-03 18:05:01 -0700

    Add iterative in-order traversal implementation in in_order_traversal.py, utilizing a stack to traverse a binary tree and return values in order.

--- Easy/in_order_traversal.py
+++ Easy/in_order_traversal.py
@@ -43,3 +43,26 @@
  
 
 # Follow up: Recursive solution is trivial, could you do it iteratively?
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        stack = []
+        result = []
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            result.append(root.val)
+            root = root.right
+        return result
