2a4100818378cc13330e9a7577f75404c721c55d
Yash-Swaminathan <yashswaminathan1@gmail.com>
2025-12-15 20:23:46 -0700

    Add generate method in pascal_triangle.py to compute Pascal's triangle for a given number of rows, enhancing functionality with a clear implementation.

--- Easy/pascal_triangle.py
+++ Easy/pascal_triangle.py
@@ -18,3 +18,20 @@
 # Constraints:
 
 # 1 <= numRows <= 30
+
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+
+        pascal = []
+
+        for i in range(numRows):
+            row = [1] * (i+1)
+            for j in range(1, i):
+                row[j] = pascal[i-1][j-1] + pascal[i-1][j]
+            pascal.append(row)
+        return pascal
+
